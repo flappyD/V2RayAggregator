5cae4fe2e899e56c3688f2f3964a979ffc194fad
defffis <36509310+defffis@users.noreply.github.com>
2023-09-08 09:22:39 +0300

    Update MY1_eternity_convert.py

--- utils/MY1_eternity_convert.py
+++ utils/MY1_eternity_convert.py
@@ -109,7 +109,7 @@ def eternity_convert(file, config, output, provider_file_enabled=True):
                 line = re.sub("name:( |)(.*?),", "name: %s | %s," %
                               (name, speed), line)
             except:
-                print(log_lines_without_bad_char[indexx])
+                #     print(log_lines_without_bad_char[indexx])
                 pass
             #           line = '  ' + line
             line = line.replace('- ', '')
